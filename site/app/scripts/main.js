0eb260fb3df284d69fc5c0ca01456f23b3e4853f
Paul Kinlan <paul.kinlan@gmail.com>
2014-10-08 10:36:42 +0100

    Adding a load more stuff to the ui

--- site/app/scripts/main.js
+++ site/app/scripts/main.js
@@ -21,35 +21,12 @@
 
   var querySelector = document.querySelector.bind(document);
 
-  var navdrawerContainer = querySelector('.navdrawer-container');
   var body = document.body;
-  var appbarElement = querySelector('.app-bar');
-  var menuBtn = querySelector('.menu');
+  
   var main = querySelector('main');
   var querybar = querySelector('#querybar');
   var container = querySelector('#container');
   var stats;
-
-  function closeMenu() {
-    body.classList.remove('open');
-    appbarElement.classList.remove('open');
-    navdrawerContainer.classList.remove('open');
-  }
-
-  function toggleMenu() {
-    body.classList.toggle('open');
-    appbarElement.classList.toggle('open');
-    navdrawerContainer.classList.toggle('open');
-    navdrawerContainer.classList.add('opened');
-  }
-
-  main.addEventListener('click', closeMenu);
-  menuBtn.addEventListener('click', toggleMenu);
-  navdrawerContainer.addEventListener('click', function (event) {
-    if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
-      closeMenu();
-    }
-  });
 
 
   var currentCmd;
