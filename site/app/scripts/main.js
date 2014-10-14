29bc343f1ac2f90b2f92778a2592d1b4ae04e3be
Paul Kinlan <paul.kinlan@gmail.com>
2014-10-14 08:11:27 +0200

    Adding some instructions

--- site/app/scripts/main.js
+++ site/app/scripts/main.js
@@ -64,37 +64,4 @@
   statsConsole.setPrompt('> ');
 
   window.addEventListener("message", receiveMessage, false);
-
-  window.addEventListener('load', function() {
-    var deviceType = 'all';
-    stats = new BrowserStats();
-
-    stats.load(deviceType, function(data) {
-
-      var allFeatures = this.getFeatures();
-
-      // Era work
-      var soon = this.getEras([3,2,1])
-      var today = this.getEras([0]);
-      var last_version = this.getEras([-1]);
-      var last_last_version = this.getEras([-2]);
-
-      var commingSoon = __.difference(soon, today);
-
-
-      var featuresInTwo = __.difference(allFeatures.chrome['38'], allFeatures.chrome['34']);
-     
-
-      var not_in_ie = __.difference(allFeatures.chrome['38'], allFeatures.ie['11']);
-      var not_in_chrome = __.difference(allFeatures.ie['11'], allFeatures.chrome['38']);
-
-      var not_in_chrome_and = __.difference(allFeatures.chrome['37'], allFeatures.and_chr['37']);
-
-
-      var not_in_ie8 = __.difference(allFeatures.ie['9'], allFeatures.ie['8']);
-      var new_features1 = __.difference(allFeatures.ie['9'], allFeatures.ie['8']);
-      var new_features = __.difference(allFeatures.ie['11'], allFeatures.ie['10']);
-      var features_in_both = __.intersection(allFeatures.ie['11'], allFeatures.ie['10']);
-    }.bind(stats));
-  });
 })();
